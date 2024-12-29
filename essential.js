77417b1ac9eaf15d7b99f3c6db6a090c19f722ea
brendanee <brendanee314@gmail.com>
2024-12-28 16:37:05 -0800

    Add smarter scrolling behavior

--- essential.js
+++ essential.js
@@ -18,7 +18,11 @@ footerNode.innerHTML =
 <a href="#">About</a>
 <div>View source code on <a href="https://github.com/brendanee/brendanee.github.io" target="_blank">Github</a></div>
 <div id="copyright">Copyright © <a href="mailto:&#109;&#101;&#64;&#98;&#114;&#101;&#110;&#100;&#97;&#110;&#46;&#101;&#101;">Brendan Ee</a> 2023-2024, All Rights Reserved.</div>
-<link rel="stylesheet" href="./essential.css"></link>
 `;
 document.querySelector('body').prepend(headerNode);
 document.querySelector('body').append(footerNode);
+// If not on main page, shrink header
+if (window.scrollY < 1 && location.pathname !== '/' && location.pathname !== '/index.html') {
+  // Probably need delay for element adding, idk
+  setTimeout(() => {window.scrollBy(0, 80)}, 10);
+}
