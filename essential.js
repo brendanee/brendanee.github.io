285cded02923b30168a6ba3d69112c19b67072ad
brendanee <brendanee314@gmail.com>
2024-12-28 17:34:36 -0800

    Added Github logo

--- essential.js
+++ essential.js
@@ -16,7 +16,7 @@ footerNode.innerHTML =
 <a href="#">Code</a>
 <a href="#">Photos</a>
 <a href="#">About</a>
-<div>View source code on <a href="https://github.com/brendanee/brendanee.github.io" target="_blank">Github</a></div>
+<div>View source code on <a href="https://github.com/brendanee/brendanee.github.io" target="_blank"><img style="vertical-align: middle;" src="./assets/github.svg"></a></div>
 <div id="copyright">Copyright © <a href="mailto:&#109;&#101;&#64;&#98;&#114;&#101;&#110;&#100;&#97;&#110;&#46;&#101;&#101;">Brendan Ee</a> 2023-2024, All Rights Reserved.</div>
 `;
 document.querySelector('body').prepend(headerNode);
