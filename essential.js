e29b42c1b1598dd2e923c3ef3851dd96017694d9
brendanee <brendanee314@gmail.com>
2025-02-21 03:01:29 -0800

    Made Photo layout better, cleanup

--- essential.js
+++ essential.js
@@ -16,7 +16,7 @@ footerNode.innerHTML =
 <a href="https://github.com/brendanee">Code</a>
 <a href="/photos">Photos</a>
 <a href="#">About</a>
-<div>View source code on <a href="https://github.com/brendanee/brendanee.github.io" target="_blank"><img style="vertical-align: middle;" src="./assets/github.svg"></a></div>
+<div>View source code on <a aria-label="Go to GitHub" href="https://github.com/brendanee/brendanee.github.io" target="_blank"><img style="vertical-align: middle;" src="./assets/github.svg" alt="The GitHub logo"></a></div>
 <div id="copyright">Copyright © <a href="mailto:&#109;&#101;&#64;&#98;&#114;&#101;&#110;&#100;&#97;&#110;&#46;&#101;&#101;">Brendan Ee</a> 2023-2024, All Rights Reserved.</div>
 `;
 document.querySelector('body').prepend(headerNode);
