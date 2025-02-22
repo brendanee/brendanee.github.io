60013956e899ef31676c42ff4b86d30f03c5fd80
brendanee <brendanee314@gmail.com>
2025-02-21 20:21:42 -0800

    Website for mobile part 2

--- essential.js
+++ essential.js
@@ -14,10 +14,9 @@ footerNode.innerHTML =
 <a href="/">Home</a>
 <a href="#">Lego</a>
 <a href="https://github.com/brendanee">Code</a>
-<a href="/photos">Photos</a>
+<a href="/photos.html">Photos</a>
 <a href="#">About</a>
-<div>View source code on <a aria-label="Go to GitHub" href="https://github.com/brendanee/brendanee.github.io" target="_blank"><img style="vertical-align: middle;" src="./assets/github.svg" alt="The GitHub logo"></a></div>
-<div id="copyright">Copyright © <a href="mailto:&#109;&#101;&#64;&#98;&#114;&#101;&#110;&#100;&#97;&#110;&#46;&#101;&#101;">Brendan Ee</a> 2023-2024, All Rights Reserved.</div>
+<div id="copyright">Copyright © <a href="mailto:&#109;&#101;&#64;&#98;&#114;&#101;&#110;&#100;&#97;&#110;&#46;&#101;&#101;">Brendan Ee</a> 2023-2025, All Rights Reserved.</div>
 `;
 document.querySelector('body').prepend(headerNode);
 document.querySelector('body').append(footerNode);
