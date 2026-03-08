084be7ec537db506e871f61d5e86a656865f182a
brendanee <brendanee314@gmail.com>
2026-03-07 17:15:10 -0800

    Many tweaks to get sitemap working

--- essential.js
+++ essential.js
@@ -1,25 +1,25 @@
-let headerNode = document.createElement('header');
-headerNode.innerHTML =
+const HEADER = document.createElement('header');
+HEADER.innerHTML =
 `
 <div id="shadow-cover"></div>
-<a href="/lego">LEGO</a>
-<a href="/coding">Coding</a>
+<a href="/lego/">LEGO</a>
+<a href="/coding/">Coding</a>
 <a id="flower" href="/"><img src="/assets/flower.webp" alt="Bright pink flowers against a light-blue background"></a>
-<a href="/photos">Photos</a>
-<a href="/design">Design</a>
+<a href="/photos/">Photos</a>
+<a href="/design/">Design</a>
 `;
-let footerNode = document.createElement('footer');
-footerNode.innerHTML =
+const FOOTER = document.createElement('footer');
+FOOTER.innerHTML =
 `
 <a href="/">Home</a>
-<a href="/lego">LEGO</a>
-<a href="/coding">Coding</a>
-<a href="/photos">Photos</a>
-<a href="/design">Design</a>
+<a href="/lego/">LEGO</a>
+<a href="/coding/">Coding</a>
+<a href="/photos/">Photos</a>
+<a href="/design/">Design</a>
 <div id="copyright">Copyright © <a href="mailto:&#109;&#101;&#64;&#98;&#114;&#101;&#110;&#100;&#97;&#110;&#46;&#101;&#101;">Brendan Ee</a> 2023-2025, All Rights Reserved.</div>
 `;
-document.querySelector('body').prepend(headerNode);
-document.querySelector('body').append(footerNode);
+document.querySelector('body').prepend(HEADER);
+document.querySelector('body').append(FOOTER);
 // If not on main page, shrink header
 if (window.scrollY < 1 && location.pathname !== '/' && location.pathname !== '/index.html') {
   // Probably need delay for element adding, idk
