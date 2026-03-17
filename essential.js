069aeb64f7400c60dea9deef2624ce5f8049e88c
brendanee <brendanee314@gmail.com>
2026-03-16 17:42:02 -0700

    Update copyright

--- essential.js
+++ essential.js
@@ -16,7 +16,7 @@ FOOTER.innerHTML =
 <a href="/coding/">Coding</a>
 <a href="/photos/">Photos</a>
 <a href="/design/">Design</a>
-<div id="copyright">Copyright © <a href="mailto:&#109;&#101;&#64;&#98;&#114;&#101;&#110;&#100;&#97;&#110;&#46;&#101;&#101;">Brendan Ee</a> 2023-2025, All Rights Reserved.</div>
+<div id="copyright">Copyright © <a href="mailto:&#109;&#101;&#64;&#98;&#114;&#101;&#110;&#100;&#97;&#110;&#46;&#101;&#101;">Brendan Ee</a> 2023-2026, All Rights Reserved.</div>
 `;
 document.querySelector('body').prepend(HEADER);
 document.querySelector('body').append(FOOTER);
